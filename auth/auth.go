a0af6d373791a09aab4306385a2de16a4f9f14d0
marcoswlrich <marcosapw.dev@gmail.com>
2023-11-20 02:12:05 -0400

    Add Auth api process

---
+++ auth/auth.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
+type TokenJSON struct {
+	Sub       string
+	Event_Id  string
+	Token_use string
+	Scope     string
+	Auth_time int
+	Iss       string
+	Exp       int
+	Iat       int
+	Client_id string
+	Username  string
+}
+
+func ValidateToken(token string) (bool, error, string) {
+	parts := strings.Split(token, ".")
+
+	if len(parts) != 3 {
+		fmt.Println("Token invalido ")
+		return false, nil, "Token invalido"
+	}
+
+	userInfo, err := base64.StdEncoding.DecodeString(parts[1])
+	if err != nil {
+		fmt.Println("Nao foi possivel decodificar o Token: ", err.Error())
+		return false, err, err.Error()
+	}
+
+	var tkj TokenJSON
+	err = json.Unmarshal(userInfo, &tkj)
+	if err != nil {
+		fmt.Println("Nao foi possivel decodificar a estrutura JSON", err.Error())
+		return false, err, err.Error()
+	}
+
+	thour := time.Now()
+	tm := time.Unix(int64(tkj.Exp), 0)
+
+	if tm.Before(thour) {
+		fmt.Println("Tempo de expiracao token = " + tm.String())
+		fmt.Println("Token expirado!")
+		return false, err, "Token expirado !!"
+	}
+
+	return true, nil, string(tkj.Username)
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/marcoswlrich/ecombrutus/auth"
 )
 
 func Handlers(
@@ -19,5 +21,116 @@ func Handlers(
 	id := request.PathParameters["id"]
 	idn, _ := strconv.Atoi(id)
 
+	isOk, statusCode, user := validateAuth(path, method, headers)
+	if !isOk {
+		return statusCode, user
+	}
+
+	switch path[0:4] {
+	case "user":
+		return ProcessUsers(body, path, method, user, id, request)
+	case "prod":
+		return ProcessProducts(body, path, method, user, idn, request)
+	case "stoc":
+		return ProcessStock(body, path, method, user, idn, request)
+	case "addr":
+		return ProcessAddress(body, path, method, user, idn, request)
+	case "cate":
+		return ProcessCategory(body, path, method, user, idn, request)
+	case "orde":
+		return ProcessOrder(body, path, method, user, idn, request)
+	}
+
 	return 400, "Method Invalid"
+}
+
+func validateAuth(path string, method string, headers map[string]string) (bool, int, string) {
+	if (path == "product" && method == "GET") || (path == "category" && method == "GET") {
+		return true, 200, ""
+	}
+
+	token := headers["authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token requerido"
+	}
+
+	todoOk, err, msg := auth.ValidateToken(token)
+	if !todoOk {
+		if err != nil {
+			fmt.Println("Error Token" + err.Error())
+			return false, 401, err.Error()
+		} else {
+			fmt.Println("Error Token" + msg)
+			return false, 401, msg
+		}
+	}
+
+	fmt.Println("Token Ok")
+	return true, 200, msg
+}
+
+func ProcessUsers(
+	body string,
+	path string,
+	method string,
+	user string,
+	id string,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
+}
+
+func ProcessProducts(
+	body string,
+	path string,
+	method string,
+	user string,
+	id int,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
+}
+
+func ProcessCategory(
+	body string,
+	path string,
+	method string,
+	user string,
+	id int,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
+}
+
+func ProcessStock(
+	body string,
+	path string,
+	method string,
+	user string,
+	id int,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
+}
+
+func ProcessAddress(
+	body string,
+	path string,
+	method string,
+	user string,
+	id int,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
+}
+
+func ProcessOrder(
+	body string,
+	path string,
+	method string,
+	user string,
+	id int,
+	request events.APIGatewayV2HTTPRequest,
+) (int, string) {
+	return 400, "Metodo invalido"
 }
